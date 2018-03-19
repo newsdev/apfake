9c24ac1f5d8b0db19750ee57c588bfcb79d6129d
Jeremy Bowers <jeremyjbowers@gmail.com>
2018-03-19 10:30:11 -0400

    Well that was a clear error message

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='apfake',
     version='0.0.1',
     author='Jeremy Bowers',
-    author_email='jeremy.bowers@nytimes.com, deads@npr.org',
+    author_email='jeremy.bowers@nytimes.com',
     url='https://github.com/newsdev/apfake',
     description='Command-line interface for extrapolating test data from a single AP API JSON file.',
     long_description=read('README.rst'),
@@ -28,12 +28,12 @@ setup(
     license="Apache License 2.0",
     keywords='election race candidate democracy news associated press',
     install_requires=['ujson==1.35'],
-    classifiers=(
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-    )
+        'Programming Language :: Python :: 3.6'
+    ]
 )
